bdbc1bf97f52d1383466554bf7403d10906a843d
Gabriel Barros Feitosa Sá <gabreilbarros13@gmail.com>
2023-04-11 01:16:14 -0300

    Removendo de arvore binaria

--- ArvoreBinaria-TREE/main.c
+++ ArvoreBinaria-TREE/main.c
@@ -3,59 +3,152 @@
 
 struct node
 {
-  int valor;
+  int value;
   struct node *left;
   struct node *right;
 };
 
 typedef struct node Node;
 
-Node *create(int valor)
+Node *create(int value)
 {
   Node *tree = (Node *)malloc(sizeof(Node));
-  tree->valor = valor;
+  tree->value = value;
   tree->left = NULL;
   tree->right = NULL;
 
   return tree;
 }
 
-void print(Node *tree)
+void print(Node *root, int depth)
 {
-  if (tree == NULL)
+  if (root == NULL)
   {
     return;
   }
 
-  printf("Valor: %d\n", tree->valor);
-  print(tree->left);
-  print(tree->right);
+  // Imprime a subarvore direita
+  print(root->right, depth + 1);
+
+  // Imprime a raiz
+  for (int i = 0; i < depth; i++)
+  {
+    printf("   ");
+  }
+  printf("└──");
+  printf("%d\n", root->value);
+
+  // Imprime a subarvore esquerda
+  print(root->left, depth + 1);
 }
 
-void add(Node *tree, int valor)
+void printInOrder(Node *tree)
 {
-  if (valor < tree->valor)
+  if (tree == NULL)
+    return;
+
+  printInOrder(tree->left);
+  printf("%d ", tree->value);
+  printInOrder(tree->right);
+}
+
+void add(Node *tree, int value)
+{
+  if (tree == NULL)
+  {
+    tree = create(value);
+    return;
+  }
+
+  if (value < tree->value)
   {
     if (tree->left == NULL)
-      tree->left = create(valor);
+      tree->left = create(value);
     else
-      add(tree->left, valor);
+      add(tree->left, value);
   }
   else
   {
     if (tree->right == NULL)
-      tree->right = create(valor);
+      tree->right = create(value);
     else
-      add(tree->right, valor);
+      add(tree->right, value);
   }
+}
+
+Node *pop(Node *tree, int value)
+{
+  if (tree == NULL)
+    return NULL;
+
+  if (value < tree->value)
+    tree->left = pop(tree->left, value);
+  else if (value > tree->value)
+    tree->right = pop(tree->right, value);
+  else if (value == tree->value)
+  {
+    if (tree->left == NULL && tree->right == NULL)
+    {
+      free(tree);
+      return NULL;
+    }
+
+    if (tree->left != NULL && tree->right == NULL)
+    {
+      Node *aux = tree->left;
+      free(tree);
+      return aux;
+    }
+
+    if (tree->left == NULL && tree->right != NULL)
+    {
+      Node *aux = tree->right;
+      free(tree);
+      return aux;
+    }
+
+    if (tree->left != NULL && tree->right != NULL)
+    {
+      Node *aux = tree->right;
+      while (aux->left != NULL)
+      {
+        aux = aux->left;
+      }
+      tree->value = aux->value;
+      tree->right = pop(tree->right, aux->value);
+      return tree;
+    }
+  }
+  return tree;
 }
 
 int main()
 {
-  Node *tree = create(1);
-  add(tree, 2);
+  Node *tree = create(10);
+  add(tree, 5);
+  add(tree, 15);
+  add(tree, 17);
   add(tree, 3);
-  add(tree, 4);
-  print(tree);
+  add(tree, 7);
+  add(tree, 8);
+  add(tree, 6);
+  add(tree, 9);
+  add(tree, 12);
+  print(tree, 1);
+  printf("\n");
+  printf("\n");
+  printInOrder(tree);
+  printf("\n");
+  print(tree, 1);
+  printf("--------------\n");
+  pop(tree, 10);
+  print(tree, 1);
+  printInOrder(tree);
+  printf("\n");
+  pop(tree, 5);
+  printf("\n");
+  printInOrder(tree);
+  printf("\n");
+
   return 0;
 }
